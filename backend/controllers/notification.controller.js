7bfd39a065825b3814c587196c5030c4ee92b65a
Artur <exalt.webdev.97@gmail.com>
2025-06-30 12:12:18 +0200

    5-prepared app for deployment+remove functionality to deleting one notification

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -30,28 +30,3 @@ export const deleteNotifications = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
-
-export const deleteOneNotification = async (req, res) => {
-  try {
-    const notificationId = req.params.id;
-    const userId = req.user._id;
-    const notification = await Notification.findById(notificationId);
-
-    if (!notification)
-      return res.status(404).json({ message: "Notification not found" });
-
-    if (notification.to.toString() !== userId.toString())
-      return res
-        .status(403)
-        .json({
-          message: "You are not authorized to delete this notification",
-        });
-
-    await Notification.findByIdAndDelete(notificationId);
-
-    res.status(200).json({ message: "Notification deleted successfully" });
-  } catch (error) {
-    console.log("Error in deleteOneNotification: ", error.message);
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
--- backend/routes/notification.route.js
+++ backend/routes/notification.route.js
@@ -2,7 +2,6 @@ import express from "express";
 import {
   getNotifications,
   deleteNotifications,
-  deleteOneNotification,
 } from "../controllers/notification.controller.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 
@@ -10,6 +9,5 @@ const router = express.Router();
 
 router.get("/", protectRoute, getNotifications);
 router.delete("/", protectRoute, deleteNotifications);
-router.delete("/:id", protectRoute, deleteOneNotification);
 
 export default router;
